54584de6598d3e625ddcbde920edd651f5d1430d
Alex Davis <alx@juxt.pro>
2021-09-09 14:00:04 +0100

    Fix prod css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 module.exports = {
-    mode: 'jit',
-    purge: [
-        './resources/public/js/compiled/*.js',
-        './resources/public/*.html'
-    ],
-    darkMode: 'false',
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-            cursor: ['disabled']
-        }
-    },
-    theme: {
-        extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
-            },
-        }
-    },
-    plugins: [],
+  mode: 'jit',
+  purge: [
+    './resources/public/js/compiled/app.js',
+  ],
+  darkMode: 'false',
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled']
+    }
+  },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
+      },
+    }
+  },
+  plugins: [],
 }
